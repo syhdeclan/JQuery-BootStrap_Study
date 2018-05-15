4b7fb5cea4f51c6282e9c068ed45fe5d6310f205
syhdeclan <38597521+syhdeclan@users.noreply.github.com>
2018-05-15 16:08:12 +0800

    Add files via upload

---
+++ SkinSystem.js
@@ -0,0 +1,220 @@
+var avatarString =
+    "http://img5.dwstatic.com/lol/1310/245353650426/1381398467886.png\n" +
+    "http://pic3.duowan.com/lol/1208/210247342385/210247356941.png\n" +
+    "http://img2.dwstatic.com/lol/1607/333467056501/1469511859510.png\n" +
+    "http://img4.dwstatic.com/lol/1602/320083658274/1456128461876.png\n" +
+    "http://img.dwstatic.com/lol/1511/311181024943/1447226977586.png\n" +
+    "http://img3.dwstatic.com/lol/1408/273091424599/1409136231951.png\n" +
+    "http://img.dwstatic.com/lol/1405/264077743227/1400123196470.png\n" +
+    "http://img4.dwstatic.com/lol/1311/249491473153/1385536285032.png\n" +
+    "http://img4.dwstatic.com/lol/1308/240574668479/1376619482925.png\n" +
+    "http://img1.dwstatic.com/lol/1306/235048838354/1371093644361.png\n" +
+    "http://img.dwstatic.com/lol/1305/231871544650/231871561051.png\n" +
+    "http://img5.dwstatic.com/lol/1303/228242985967/228242993255.png\n" +
+    "http://img3.dwstatic.com/lol/1302/225381420352/225382419902.png\n" +
+    "http://img.dwstatic.com/lol/1301/222714364061/222714396868.png\n" +
+    "http://pic2.duowan.com/lol/1212/219251307713/219251317038.png\n" +
+    "http://pic5.duowan.com/lol/1210/215627370104/215627419666.png\n" +
+    "http://img2.dwstatic.com/lol/1411/279647578275/1415693006769.png\n" +
+    "http://pic.duowan.com/lol/1207/207224467449/207225320108.png\n" +
+    "http://pic.duowan.com/lol/1207/205773506081/205773517075.png\n" +
+    "http://pic2.duowan.com/lol/1206/202484172754/202487208149.png\n" +
+    "http://img5.dwstatic.com/lol/1605/327431848757/1463477081676.png\n" +
+    "http://img2.dwstatic.com/lol/1407/269890592132/1405936914678.png\n" +
+    "http://pic4.duowan.com/lol/1203/196696818878/196703018435.png\n" +
+    "http://img3.dwstatic.com/lol/1508/304523821745/1440568627197.png\n" +
+    "http://img3.dwstatic.com/lol/1407/269890592132/1405937100681.png\n" +
+    "http://img3.dwstatic.com/lol/1410/278074218098/1414119027670.png\n" +
+    "http://img4.dwstatic.com/lol/1306/235591678174/1371636483394.png\n" +
+    "http://img2.dwstatic.com/lol/1407/269890592132/1405935685198.png\n" +
+    "http://img2.dwstatic.com/lol/1407/269890592132/1405937171546.png\n" +
+    "http://img2.dwstatic.com/lol/1409/275653745481/1411698550859.png\n" +
+    "http://img4.dwstatic.com/lol/1411/279647578275/1415692717334.png\n" +
+    "http://img1.dwstatic.com/lol/1310/246296550427/1382341365723.png\n" +
+    "http://img2.dwstatic.com/lol/1411/279649187628/1415694426798.png\n" +
+    "http://img3.dwstatic.com/lol/1411/279649187628/1415694802272.png\n" +
+    "http://pic2.duowan.com/lol/1205/200657192402/200657210799.png\n" +
+    "http://img2.dwstatic.com/lol/1407/269890592132/1405937405275.png\n" +
+    "http://img4.dwstatic.com/lol/1605/327431848757/1463476917030.png\n" +
+    "http://img5.dwstatic.com/lol/1310/246295394773/1382341114754.png\n" +
+    "http://img2.dwstatic.com/lol/1605/327431848757/1463476767064.png\n" +
+    "http://img4.dwstatic.com/lol/1305/231871544650/231873618947.png\n" +
+    "http://pic5.duowan.com/lol/1205/200657192402/200657210786.png\n" +
+    "http://img4.dwstatic.com/lol/1409/275654100149/1411698909355.png\n" +
+    "http://img.dwstatic.com/lol/1407/269517769163/1405562576193.png\n" +
+    "http://img3.dwstatic.com/lol/1409/275654100149/1411698914143.png\n" +
+    "http://img2.dwstatic.com/lol/1306/235155745723/1371200551407.png\n" +
+    "http://img5.dwstatic.com/lol/1411/279647578275/1415692869825.png\n" +
+    "http://img5.dwstatic.com/lol/1410/276707403146/1412752216828.png\n" +
+    "http://img3.dwstatic.com/lol/1411/279647578275/1415693077770.png\n" +
+    "http://img2.dwstatic.com/lol/1410/276707403146/1412752590005.png\n" +
+    "http://img2.dwstatic.com/lol/1410/277214494112/1413259306099.png\n" +
+    "http://img2.dwstatic.com/lol/1310/246296550427/1382341365660.png\n" +
+    "http://img1.dwstatic.com/lol/1407/269890592132/1405937554667.png\n" +
+    "http://img4.dwstatic.com/lol/1411/279649187628/1415694680759.png\n" +
+    "http://img4.dwstatic.com/lol/1411/279649187628/1415694348156.png\n" +
+    "http://img4.dwstatic.com/lol/1407/269890592132/1405935684932.png\n" +
+    "http://img2.dwstatic.com/lol/1407/269890592132/1405937846531.png\n" +
+    "http://img2.dwstatic.com/lol/1507/300369495508/1436414308812.png\n" +
+    "http://img5.dwstatic.com/lol/1410/276707403146/1412752216963.png\n" +
+    "http://img3.dwstatic.com/lol/1407/269890592132/1405938229808.png\n" +
+    "http://img1.dwstatic.com/lol/1605/327431848757/1463476955213.png\n" +
+    "http://img2.dwstatic.com/lol/1411/279647578275/1415692934837.png\n" +
+    "http://img3.dwstatic.com/lol/1605/327431848757/1463477136452.png\n" +
+    "http://img4.dwstatic.com/lol/1310/246295394773/1382341114833.png\n" +
+    "http://img1.dwstatic.com/lol/1305/231871544650/231872031019.png\n" +
+    "http://img2.dwstatic.com/lol/1310/246296550427/1382341365515.png\n" +
+    "http://img5.dwstatic.com/lol/1605/327431848757/1463476681702.png\n" +
+    "http://img3.dwstatic.com/lol/1310/246295394773/1382341114670.png\n" +
+    "http://img2.dwstatic.com/lol/1310/246296550427/1382341588085.png\n" +
+    "http://img1.dwstatic.com/lol/1310/246295394773/1382341114450.png\n" +
+    "http://img1.dwstatic.com/lol/1411/279647578275/1415692397219.png\n" +
+    "http://img1.dwstatic.com/lol/1305/231871544650/231872754143.png\n" +
+    "http://img2.dwstatic.com/lol/1405/264160344403/1400205147348.png\n" +
+    "http://img5.dwstatic.com/lol/1605/327431848757/1463477413939.png\n" +
+    "http://img5.dwstatic.com/lol/1407/269888611908/1405934265741.png\n" +
+    "http://img2.dwstatic.com/lol/1605/327431848757/1463476821956.png\n" +
+    "http://img5.dwstatic.com/lol/1410/276710213760/1412756511788.png\n" +
+    "http://img2.dwstatic.com/lol/1410/277753050551/1413797864676.png\n" +
+    "http://pic2.duowan.com/lol/1205/200657192402/200657210836.png\n" +
+    "http://img2.dwstatic.com/lol/1411/279649187628/1415694905444.png\n" +
+    "http://img1.dwstatic.com/lol/1305/231871544650/231873225306.png\n" +
+    "http://img3.dwstatic.com/lol/1605/327431848757/1463477047695.png\n" +
+    "http://img1.dwstatic.com/lol/1407/269606339574/1405651153711.png\n" +
+    "http://img4.dwstatic.com/lol/1411/279649187628/1415693994078.png\n" +
+    "http://img4.dwstatic.com/lol/1410/276707403146/1412757458195.png\n" +
+    "http://img4.dwstatic.com/lol/1410/277214494112/1413259306011.png\n" +
+    "http://img.dwstatic.com/lol/1411/279649187628/1415694612797.png\n" +
+    "http://img1.dwstatic.com/lol/1410/276707403146/1412752217106.png\n" +
+    "http://img5.dwstatic.com/lol/1605/327431848757/1463476890582.png\n" +
+    "http://img1.dwstatic.com/lol/1605/327431848757/1463476853235.png\n" +
+    "http://img4.dwstatic.com/lol/1305/231871544650/231873517836.png\n" +
+    "http://img.dwstatic.com/lol/1411/279647578275/1415692576539.png\n" +
+    "http://img1.dwstatic.com/lol/1411/279647578275/1415692632471.png\n" +
+    "http://img3.dwstatic.com/lol/1407/269888611908/1405934460434.png\n" +
+    "http://img.dwstatic.com/lol/1411/279649187628/1415694139441.png\n" +
+    "http://pic5.duowan.com/lol/1205/200657192402/200657210853.png\n" +
+    "http://img4.dwstatic.com/lol/1407/269888611908/1405934827815.png\n" +
+    "http://img2.dwstatic.com/lol/1703/353430828133/1489478217150.png\n" +
+    "http://img3.dwstatic.com/lol/1407/269517769163/1405562754544.png\n" +
+    "http://img2.dwstatic.com/lol/1611/344428862745/1480473717181.png\n" +
+    "http://img1.dwstatic.com/lol/1609/338393194134/1474437997425.png\n" +
+    "http://img2.dwstatic.com/lol/1407/269888611908/1405934761751.png";
+
+var avatarList = avatarString.split('\n');
+var count = 0;
+var chosenHero = 0;
+for (let avatar of avatarList) {
+    let avatarImg = $('<img src=""style="cursor: pointer">');
+    avatarImg.addClass("newImg-thumbnail ");
+    avatarImg.attr({"src": avatar, "width": "100px", "height": '100px'});
+    $("#avatar").append(avatarImg);
+    avatarImg.click(function () {
+        chosenHero = avatarList.indexOf(avatar);
+        updateSkins(chosenHero);
+        console.log("选中英雄：" + chosenHero);
+    });
+    if (localStorage.getItem('skin' + count)) {
+        //存在则不动
+    } else {
+        localStorage.setItem('skin' + count, JSON.stringify([]));
+        console.log("第" + count + "条建立");
+    }
+    count++;
+}
+
+
+//更新皮肤轮播
+function updateSkins(chosenHero) {
+    //删除原有的
+    $('#skinDiv').empty();
+    $('#skinUl').empty();
+    let skinList = JSON.parse(localStorage.getItem('skin' + chosenHero));
+    var skinDiv = document.getElementById('skinDiv');
+    //如果有皮肤就进去
+    if (skinList) {
+        for (var skin of skinList) {
+            //添加元素和图片
+            let newSkin = document.createElement('div');
+            let newSkinImg = document.createElement('img');
+            newSkin.classList.add('carousel-item');
+            newSkinImg.setAttribute('src', skin);
+            newSkinImg.classList.add('skinImg');
+            newSkin.appendChild(newSkinImg);
+            skinDiv.appendChild(newSkin);
+            let num = skinList.indexOf(skin);
+            //添加下标
+            // $('<li>', {
+            //     'data-target': "#demo",
+            //     'data-slide-to': num,
+            //     'class': 'active'
+            // }).appendTo($('ul'));
+            var li = $('<li data-target="#demo">');
+            li.attr('data-slide-to', num);
+            $('ul').append(li);
+
+            //默认激活
+            if (num == 0) {
+                newSkin.classList.add('active');
+                li.addClass('active');
+            }
+        }
+    }
+
+}
+
+//初始化皮肤
+$().ready(updateSkins(chosenHero));
+
+$('#localBtn').click(function () {
+    $('#skinSrc').hide();
+    $('#skinSrc').val('');
+    $('#baseInput').show();
+
+});
+$('#picUrlBtn').click(function () {
+    $('#skinSrc').show();
+    $('#baseInput').hide();
+    $('#baseInput').val('');
+});
+
+//添加皮肤按钮
+$('#addSkin').click(function () {
+    let baseInput = document.getElementById('baseInput');
+    var heroSkins = JSON.parse(localStorage.getItem('skin' + chosenHero));
+    if ($('#skinSrc').val()) {
+        if (/^(\.png)|(\.jpg)$/.test($('#skinSrc').val())) {
+            //正则
+            heroSkins.push($('#skinSrc').val());
+            localStorage.setItem('skin' + chosenHero, JSON.stringify(heroSkins));
+            console.log(chosenHero + "添加了皮肤");
+            alert('添加成功');
+        }else{
+            alert('这好像不是图片地址吧？');
+        }
+    } else if (baseInput.files) {
+        var file = baseInput.files[0];
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            console.log(reader.result)
+            heroSkins.push(reader.result);
+            localStorage.setItem('skin' + chosenHero, JSON.stringify(heroSkins));
+        }
+        alert('添加成功');
+    }
+});
+$('#delSkin').click(function () {
+    if (confirm('确定要删除该皮肤吗？')) {
+        let skinList = $('#skinDiv div');
+        $.each(skinList, function () {
+            if ($(this).hasClass('active')) {
+                let deleteSkin = skinList.index($(this));
+                let JSONSkinList = JSON.parse(localStorage.getItem('skin' + chosenHero));
+                JSONSkinList.splice(deleteSkin, 1);
+                localStorage.setItem('skin' + chosenHero, JSON.stringify(JSONSkinList));
+                console.log('删除了' + chosenHero + '英雄的' + deleteSkin + '皮肤');
+            }
+        });
+    }
+});
